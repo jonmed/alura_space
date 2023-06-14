b53aa80a6ceeef53aa62057b42fe508a7715f15e
Jonatas Medeiros <jonatasmedeiros@servidor.uepb.edu.br>
2023-06-14 16:25:24 -0300

    finalizando curso de autenticacao

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
+from usuarios.forms import LoginForms, CadastroForms
 
 
 def login(request):
-    return render(request, 'usuarios/login.html')
+    form = LoginForms()
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+            usuario = auth.authenticate(request, username=nome, password=senha)
+            if usuario:
+                auth.login(request, usuario)
+                messages.success(request, f'{nome} logado com sucesso!')
+                return redirect('index')
+            messages.error(request, 'Erro ao efetuar login!')
+            return redirect('login')
+    return render(request, 'usuarios/login.html', {'form': form})
 
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    form = CadastroForms()
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+        if form.is_valid():
+            nome = form['nome_cadastro'].value()
+            email = form['email'].value()
+            senha = form['senha_1'].value()
+            if User.objects.filter(username=nome).exists():
+                messages.error(request, 'Usuário já existente!')
+                return redirect('cadastro')
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            messages.success(request, f'Cadastro efetuado com sucesso!')
+            return redirect('login')
+    return render(request, 'usuarios/cadastro.html', {'form': form})
+
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'Logout efetuado com sucesso!')
+    return redirect('login')
